79165162c088d8f9fe5ae09115e87728ca0f1f17
Rajukr12 <rajukuntmal@gmail.com>
2019-09-15 15:11:49 +0530

    publishing
    abc

---
+++ index.php
@@ -0,0 +1,131 @@
+<html>
+<div  style="border-style:double"  >
+<title>resume</title>
+<head>
+
+<style>
+pre{
+	font-size:140%;
+	
+}
+h2{
+	border-style:solid;
+	border-width:1px;
+	background-color:#ADD8E6;
+} 
+div.a,p.z,pre.abc{
+	padding-right:20px;
+	padding-left:20px;
+}
+table,th,td{
+	border:1px solid black;
+	border-collapse:collapse;
+}
+pre.abc{
+	font-family:Times New Roman;
+	font-size:17px;
+}
+</style>
+</head>
+<body>
+
+<pre class="abc" style="font-size:20px">
+  <strong>Raju K R
+  Bhagya Jyothi Nagar,Molakalmuru Tq.,
+  Chitradurga Dist.,Karnataka-577535
+  rajukuntmal@gmail.com
+  Contact No:9742006117</strong></pre>
+<div class="a"><h2>CAREER OBJECTIVE</h2></div> 
+<p class="z">To secure a responsible career oppurtunity to fully utilize my training and skills,while making a significant  contribution to the success of the company.</p> 
+<div class="a"><h2>EDUCATIONAL QUALIFICATION</h2></div>
+<div class="a">
+<table>
+<tr>
+<th>SL.NO</th>
+<th>DEGREE</th>
+<th>UNIVRSITY/BOARD</th>
+<th>INSTITUTION STUDIED</th>
+<th>YEAR OF COMPLETION</th>
+<th>PERCENTAGE/CGPA</th>
+</tr>
+<tr>
+<td>1</td>
+<td>B.E(CSE)PURSUING</td>
+<td>VISVESHVARAYA TECHNOLOGICAL UNIVERSITY,BELGAUM</td>
+<td>SRI SAIRAM COLLEGE OF ENGINEERING,BENGALURU</td>
+<td>2020</td>
+<td>6.2(CGPA UPTO 7th SEMESTER)</td>
+</tr>
+<tr>
+<td>2</td>
+<td>PUC</td>
+<td>DEPARTMENT OF PRE-UNIVERSITY EDUCATION</td>
+<td>SRI CHAITANYA PU COLLEGE,BELLARY</td>
+<td>2016</td>
+<td>78.5%</td>
+</tr>
+<tr>
+<td>3</td>
+<td>SSLC</td>
+<td>KARNATAKA SECONDARY EDUCATION EXAMINATION BOARD</td>
+<td>GOVT.JUNIOR COLLEGE,MOLAKALMURU</td>
+<td>2014</td>
+<td>77.28%</td>
+</tr>
+</table>
+</div>
+<div class="a"><h2>TECHNICAL SKILLS</h2></div>
+<ul>
+<li><strong>Programming languages:</strong>Basics of C,C++,SQL,Java,Python,C#,PHP,HTML</li>
+<li><strong>Operating Systems:</strong>Windows 7,10 platform,Ubuntu</li>
+<li><strong>Tools Used:</strong>Visual Studio,Android Studio</li></ul>
+<div class="a"><h2>STRENGTHS</h2></div>
+<ul>
+<li>Good in team working</li>
+<li>Quick learner</li></ul>
+<div class="a"><h2>MEMBERSHIPS</h2></div>
+<ul>
+<li>Indian Society for Technical Education (ISTE) : KA0562963 </li></ul>
+<div class="a"><h2>PROJECTS</h2></div>
+<ul>
+
+<li><pre class="abc"><strong>Working on Project</strong>
+<strong>Title:</strong>"Automated watering of plants using app"
+<strong>team members:</strong>4
+<strong>Role:</strong>team member
+<strong>Description:</strong> our project is about watering of plants using raspberry pi controlled using app.The amount of water to supplied is calculated 
+automatically based on type of plant using database sources.
+</pre></li>
+
+<li><pre class="abc"><strong>Completed Project</strong>
+<strong>Title:</strong>"Student feedback system"
+<strong>team members:</strong>4
+<strong>Role:</strong>team member
+<strong>Description:</strong> we implemented online student feedback system in our college when the traditional  method of taking student feedback for 
+faculties has been overridden by making it online process 
+</pre></li>
+</ul>
+<div class="a"><h2>EXTRA CURRICULAR ACTIVITIES</h2></div>
+<ul>Participated in <strong>"SMART INDIA HACKATHON, 2018 SOFTWARE EDITION"</strong> grand finale in Chandigarh </ul>
+<ul>Presented paper on <strong>"SMART HOSPITALS USING INTERNET OF THINGS"</strong> in i7C -2017 held at Sri Sairam College Of Engineering, Anekal, Bengaluru.</ul>
+<ul>Participated in grand finale of <strong>"MINDSPARK18 HELLA MAKEATHON"</strong> held at college of engineering Pune.</ul>
+<ul>Received certificate of completion for project <strong>"ONLINE STUDENT FEEDBACK SYSTEM"</strong> in Sri Sairam College Of Engineering, Anekal, Bengaluru</ul>
+<div class="a"><h2>PERSONAL DETAILS</h2></div>
+<ul>Father’s name   : Rangaswamy K R </ul>
+<ul>Mother’s name  : Lalitha </ul>
+<ul>Date of birth   : 21/02/1999  </ul>
+<ul>Gender     : Male </ul>
+<ul>Nationality     : Indian</ul>  
+<ul>Blood group   : B +ve </ul>
+<ul>Languages known  : English, Kannada, Telugu, Hindi</ul>
+<div class="a"><h2>DECLARATION</h2></div> 
+<p class="z">  I hereby declare that the above mentioned particulars are true to best of my knowledge and belief. </p>
+<pre class="abc">Place:Bengaluru                                                                                                                                                                                                                                                             (Raju K R)
+Date:<p id="demo"><script>
+var d = new Date();
+document.getElementById("demo").innerHTML = d;
+</script></p></pre>
+
+</body>
+</div>
+</html>
